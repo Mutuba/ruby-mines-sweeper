2be1de22e8b3b29a78863f84ed2ec5af022c807c
Mutuba <danielmutubait@gmail.com>
2024-08-02 16:46:24 +0300

    Add model tests for validation

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Game, type: :model do
   it { should have_many(:cells).dependent(:destroy) }
 
   # Validations
-  it { should define_enum_for(:level).with_values(beginner: 0, intermediate: 1, advanced: 2) }
+  it { should define_enum_for(:level).with_values(beginner: 0, intermediate: 1, advanced: 2, custom: 3) }
   it { should define_enum_for(:state).with_values(not_started: 0, in_progress: 1, won: 2, lost: 3) }
 
   it "initializes the board correctly" do
@@ -29,4 +29,13 @@ RSpec.describe Game, type: :model do
     expect(game.cells.count).to eq(81)
     expect(game.cells.where(mine: true).count).to eq(10)
   end 
+
+  context "callback" do
+    let!(:game) { create(:game, state: :in_progress)}
+    before { game.update(state: :lost)}
+
+    it "does not allow updating if game is in lost state" do
+      expect { game.update!(state: :in_progress) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: State cannot be changed once the game is lost.")
+    end
+  end
 end
